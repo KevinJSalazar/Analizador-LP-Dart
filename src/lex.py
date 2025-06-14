983a76d1940f168376edf8c15a0a6d6200004be2
RenDiego07 <dfflores@espol.edu.ec>
2025-06-13 23:24:59 -0500

    Finished adding tokens for devDiego's algorithm

--- src/lex.py
+++ src/lex.py
@@ -20,12 +20,23 @@ reserved = {
     'default': 'DEFAULT',
     'continue': 'CONTINUE',
     'export': 'EXPORT',
-    'finally': 'FINALLY'
+    'finally': 'FINALLY',
+    'double': 'DOUBLE_TYPE',
+    'int': 'INT_TYPE',
+    'num': 'NUM_TYPE',
+    'String': 'STRING_TYPE',
+    'bool': 'BOOL_TYPE',
+    'List': 'LIST_TYPE',
+    'MAP': 'MAP_TYPE'
+    
 }
 
 # Falta añadir comentarios
 
 tokens = list(reserved.values()) + [
+    'COMMENT_LINE', #regex
+    'COMMENT_BLOCK', #regex
+    'ID', #regex    
     'PLUS', #regex
     'MINUS', #regex
     'DIVIDE', #regex
@@ -52,7 +63,10 @@ tokens = list(reserved.values()) + [
     'DOT', #regex
     'COMMA', #regex
     'COLON', #regex
-]
+    'ASSIGN_OPERATOR', #regex,
+    'LBRACKET', #regex
+    'RBRACKET', #regex
+]   
 
 
 t_PLUS = r'\+'
@@ -63,6 +77,7 @@ t_AND = r'&&'
 t_OR = r'\|\|'
 t_NOT_EQUALS = r'!='
 t_NOT = r'!'
+t_ASSIGN_OPERATOR = r'='
 t_EQUALS = r'=='
 t_GREATER_THAN = r'>'
 t_LESS_THAN = r'<'
@@ -76,13 +91,36 @@ t_RBRACE = r'\}'
 t_DOT = r'\.'
 t_COMMA = r','
 t_COLON = r':'
-t_ignore = ' /'
+t_ignore = ' \t'
+t_LBRACKET = r'\['
+t_RBRACKET = r'\]'
 
+def t_COMMENT_LINE(t):
+    r'//.*'
+    return t
 
+def t_COMMENT_BLOCK(t):
+    r'/\*[\s\S]*?\*/'
+    return t
+
+def t_DOUBLE(t):
+    r'\d+.\d+'
+    t.value = float(t.value)
+    return t
 
 def t_INT(t):
     r'\d+'
     t.value = int(t.value)    
+    return t
+
+def t_BOOL(t):
+    r'true|false'
+    t.value = bool(t.value)
+    return t
+
+def t_STRING(t):
+    r'"([^"\\]|\\.)*"|\'([^\'\\]|\\.)*\''
+    t.value = t.value[1:-1] 
     return t
 
 def t_new_line(t):
@@ -101,11 +139,55 @@ def t_error(t):
 
 
 
+
 lexer = lex.lex()
 
 data = '''
-3 + 4 * 10
-  + -20 *2
+   import 'dart:math';
+    /* Este programa almacena temperaturas 
+        las filtra segun un umbal y muestra resultados.*/
+       
+    void main() {
+        final double threshold = 30.5;
+        var temperatures <double> = [28.0, 31.2, 29.5, 32.1, 27.0];
+        List<double> highTemps = [];
+
+        for(double temp in temperatures) {
+            if(temp > threshold) {
+                highTemps.add(temp);
+            }else{
+                continue;
+            }
+        }
+    }
+
+    print("Temperaturas altas detectadas:");
+    for(var t in highTemps){
+        print(" - $t °C");
+        }
+
+    final Map<String, bool> estado = {
+        "alerta": highTemps.lenght > 2,
+        "estable": highTemps.lenght <= 2
+    };
+    
+    print("\nEstado del sistema:");
+    estado.forEach((clave, valor) {
+        print("$clave: $valor");
+    });
+
+    // Declaraciones alternativas
+    const bool sistemaActivo = true;
+    late String mensaje;
+
+    if (sistemaActivo) {
+        mensaje = "Sistema operativo en línea.";
+    }
+
+    print("\nMensaje: $mensaje");
+
+    }
+
 '''
 
 lexer.input(data)
