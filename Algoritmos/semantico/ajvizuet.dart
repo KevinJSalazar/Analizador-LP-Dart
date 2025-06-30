f6b19462ba98447affeea242acf5aadf58ea681d
= <=>
2025-06-29 23:32:22 -0500

    Added Semantic testing

---
+++ Algoritmos/semantico/ajvizuet.dart
@@ -0,0 +1,25 @@
+int giveMeNumber10() {
+    int x;
+    int y;
+
+    int z = x + y; // Doesn't matter because it always return 10 hehe
+
+    return 10;
+}
+
+bool validatePurchase(int x) {
+    int x;
+    String abd = 'abc';
+    return abd;
+}
+
+int missingReturn() {
+    print("A+B");
+}
+
+void cantDefineVoidVariable() {
+    void x;
+    if (x == true) {
+        print("Hello world");
+    }
+}
