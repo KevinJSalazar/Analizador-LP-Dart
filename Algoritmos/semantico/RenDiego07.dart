8cd97177194baac2c8290648d868cab21d64935f
RenDiego07 <dfflores@espol.edu.ec>
2025-06-29 19:33:27 -0500

    uploaded logs

--- Algoritmos/semantico/RenDiego07.dart
+++ Algoritmos/semantico/RenDiego07.dart
@@ -1,10 +1,28 @@
 void main(){
     // esta bien 
     int x = 4; 
+    List<int> numeros = [3, 5, 4, 6, 9];
+    List<String> cadenas = ["diego", "flores", "rengifo"];
+    List<double> notas = [4.4 , 5.4 , 9.0];
+    int auxX = x;
+    bool flag4 = true;
+
     // debe de estar mal
-    int numero = "diego"
+    int numero = "diego";
+    int numero1 = false;
+    int numero2 = 4.3;
+    bool flag = 2;
+    bool flag2 = "string";
+    bool flag3 = 4.3;
+    List<int> numeros = ["diego", "flores"];
+    List<double> notas = [false, true, false];
+    List<String> nombres = [3, 4, 3, 2];
+    //
+
+    // esta mal porque z no se encuentra en la tabla de valores
+    int valor = z;  
+
+
     
-
-
-
+    
 }
